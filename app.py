ba73a796b7c03391cf7470e9f12e1984cee36d51
Sujey Dash <sujeydash@gmail.com>
2025-09-08 18:42:15 +0530

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
-import pickle
+import joblib
 import numpy as np
 
 # Load trained Gradient Boosting model
-with open("gradient_boosting_model.pkl", "rb") as f:
-    model = pickle.load(f)
+model = joblib.load("gradient_boosting_model.pkl")
 
 st.title("🏦 Bankruptcy Prediction App")
 st.write("Enter company risk factors to predict bankruptcy.")
